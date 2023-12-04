51da8f0db9481fcb57414e768e5f59c20a2344d3
joeldeondsouza <jdeondsouza@gmail.com>
2023-12-04 15:50:47 +0100

    added header

--- Screens/HomeScreen/index.tsx
+++ Screens/HomeScreen/index.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Image, TouchableOpacity } from "react-native";
 // Styles //
 import { styles } from "./styles";
+// Icons //
+import Icon from "react-native-vector-icons/Ionicons";
 
 const HomeScreen = () => {
-    return (
-        <View style={styles.homeConatiner}>
-            <Text>HomeScreen</Text>
-        </View>
-    )
+  return (
+    <View style={styles.homeConatiner}>
+      <View style={styles.headerConatiner}>
+        <TouchableOpacity style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../../assets/logo.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainerFilter}>
+          <Icon name="flash" size={26} color={"#2f7bb9"} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
 
 export default HomeScreen;
