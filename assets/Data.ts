b120449a2ff5b91f259cabf513a36432d36cfe6f
joeldeondsouza <jdeondsouza@gmail.com>
2023-12-05 13:30:01 +0100

    added posts dummy data

--- assets/Data.ts
+++ assets/Data.ts
@@ -20,3 +20,12 @@ export const ctg = [
     __v: 0,
   },
 ];
+
+export const posts = [
+  {
+    _id: "01",
+    ctg:"002",
+    name:"Just Started",
+    img:"https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_592,c_limit/cdb2ec36-18d6-429e-bed0-32ed2a3aff65/du-tust-es-nie-nur-f%C3%BCr-dich-just-do-it.jpg"
+  }
+];
